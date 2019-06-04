7c2ec2c1dc78645a3dfe7e4bfda43578b1c5f249
danielagutperl <daniela.gutperl@gmail.com>
2019-06-04 11:16:53 +0100

    adding Alien class, alien hit

--- js/app.js
+++ js/app.js
@@ -4,55 +4,7 @@ const squares = []
 let playerIndex = Math.floor(width * width - 3)
 let alienIndex = 0
 const bulletSpeed = 250
-
-
-
-
-
-
-
-// moves player (if allowed from valid keydown event)
-function movePlayer() {
-  squares.forEach(square => square.classList.remove('player'))
-  squares[playerIndex].classList.add('player')
-}
-
-function handleKeyDown(e) {
-  let playerShouldMove = true
-  switch(e.keyCode) {
-    case 39:
-      if (playerIndex % width < width - 1) {
-        playerIndex++
-      }
-      break
-    case 37:
-      if (playerIndex % width > 0) {
-        playerIndex--
-      }
-      break
-    case 32: 
-      shoot(playerIndex)
-    break
-      
-    default:
-      playerShouldMove = false
-  }
-  if (playerShouldMove) movePlayer()
-}
-
-function shoot(playerIndex) {
-    let bullet = playerIndex - width;
-    squares[bullet].classList.add('bullet')
-    const shootInterval = setInterval( () => {
-        squares[bullet].classList.remove('bullet')
-        bullet -= width
-        squares[bullet].classList.add('bullet')
-        if (bullet < 0) {
-            clearInterval(shootInterval)
-        }
-    }, bulletSpeed)
-
-}
+let score = 0
 
 // Initialise game
 function init() {
@@ -70,76 +22,164 @@ function init() {
   }
 
   squares[playerIndex].classList.add('player')
-  squares[alienIndex].classList.add('alien')
 
-  setInterval(moveAlien, 250);
-//   squares[shootIndex].classList.add('shoot')
+  const aliens = [
+    new Alien(0, squares),
+    new Alien(2, squares),
+    new Alien(4, squares)
+  ]
+  console.log(aliens)
+
+  // moves player (if allowed from valid keydown event)
+  function movePlayer() {
+    squares.forEach(square => square.classList.remove('player'))
+    squares[playerIndex].classList.add('player')
+  }
+
+  function handleKeyDown(e) {
+    let playerShouldMove = true
+    switch(e.keyCode) {
+      case 39:
+        if (playerIndex % width < width - 1) {
+          playerIndex++
+        }
+        break
+      case 37:
+        if (playerIndex % width > 0) {
+          playerIndex--
+        }
+        break
+      case 32:
+        shoot(playerIndex)
+        break
+
+      default:
+        playerShouldMove = false
+    }
+    if (playerShouldMove) movePlayer()
+  }
+
+  function shoot(playerIndex) {
+    let bullet = playerIndex - width
+    squares[bullet].classList.add('bullet')
+
+    const shootInterval = setInterval( () => {
+      squares[bullet].classList.remove('bullet')
+      bullet -= width
+
+      squares[bullet].classList.add('bullet')
+      if (bullet < 0) {
+        clearInterval(shootInterval)
+      }
+    }, bulletSpeed)
+
+    aliens.forEach((alien) => {
+      alien.alienHit(bullet)
+    })
+  }
+
+  // score
+
+  // let scoreDisplay = document.createElement('div')
+  // scoreDisplay.classList.add('scoreDisplay')
+  // scoreDisplay.innerHTML =
+
+
+
+
+  // setInterval(moveAlien, 1000)
+
   window.addEventListener('keydown', handleKeyDown)
 }
 
 
-window.addEventListener('DOMContentLoaded', init) 
+window.addEventListener('DOMContentLoaded', init)
 
 
 
-// add alien row
-// add shooting line - case key up add line up
-// collision function - if line in same square as alien, bust alien into asterrisk
-// if busted add to score
+// if alienHit score++
+// bullets from aliens
 
+class Alien {
+  constructor(alienIndex, squares) {
+    this.alienIndex = alienIndex
+    this.squares = squares
+    this.moveCount = 0
+    this.movingRight = true
+    this.timerId = null
+    this.isLive = true
+    this.init()
+  }
 
-// // shooting 
-// function shoot() {
-//     let shootIndex = 87
-//     squares.forEach(square => square.classList.remove('shoot'))
-//     squares[shootIndex].classList.add('shoot')
-// }
+  get alienDead() {
+    return !this.isLive
+  }
 
-// function shootKey(isShooting) {
-    // if (isShooting === true) {
-    //       playerIndex -= width
-    //     }
+  init() {
+    this.squares[this.alienIndex].classList.add('alien')
+    this.timerId = setInterval(this.moveAlien.bind(this), 1000)
+  }
 
-// const score = document.querySelector('.score')
+  moveAlien() {
+    if (!this.isLive) return // end if not live
 
-// let shootIndex = 0;
-//     if (shootIndex === alienIndex) {
-//         let score = 0
-//         score =+ 1
-//         score.innerHTML = score
-// alien movement
-// let movingRight = true
-// console.log(window);
-// setInterval(console.log('hello'), 1000)
-// 
+    this.squares[this.alienIndex].classList.remove('alien')
+    if (this.moveCount < 5) {
+      if (this.movingRight) {
+        this.alienIndex++
+      } else {
+        this.alienIndex--
+      }
+      this.moveCount++
+    } else {
+      this.alienIndex += width
+      this.moveCount = 0
+      this.movingRight =!this.movingRight
+    }
+    this.squares[this.alienIndex].classList.add('alien')
+  }
 
-function moveAlien() {
-    let moving = 1
-    squares[alienIndex].classList.remove('alien')  
-
-    if (alienIndex < 9) {
-        alienIndex = alienIndex + moving
-    } else if (alienIndex === 9) {
-        alienIndex += width
-    } else if (alienIndex <= 19 && alienIndex > 10) {
-        moving = -1
-        alienIndex = alienIndex + moving
-    } else if (alienIndex === 10) {
-        alienIndex += width 
-    } else if (alienIndex < 29 && alienIndex >= 20) {
-        moving = +1
-        alienIndex = alienIndex + moving
-    } else if (alienIndex === 29) {
-        alienIndex += width
-    } else if (alienIndex <= 39 && alienIndex > 30) {
-        moving = -1
-        alienIndex = alienIndex + moving
-    } else if (alienIndex === 30) {
-        alienIndex += width 
-    } else if (alienIndex < 49 && alienIndex >= 40) {
-        moving = +1
-        alienIndex = alienIndex + moving
+  alienHit(bulletIndex) {
+    if (bulletIndex === this.alienIndex) {
+      this.isLive = false
+      this.squares[this.alienIndex].classList.remove('alien')
+      return true
     }
 
-    squares[alienIndex].classList.add('alien')  
+    return false
+  }
 }
+
+
+//
+// const rowsCount = 1
+// function moveAlien() {
+//   let moving = 1
+//   squares[alienIndex].classList.remove('alien')
+//
+//   if (alienIndex < 9) {
+//     alienIndex = alienIndex + moving
+//   } else if (alienIndex === 9) {
+//     alienIndex += width
+//   } else if (alienIndex <= 19 && alienIndex > 10) {
+//     moving = -1
+//     alienIndex = alienIndex + moving
+//   } else if (alienIndex === 10) {
+//     alienIndex += width
+//   } else if (alienIndex < 29 && alienIndex >= 20) {
+//     moving = +1
+//     alienIndex = alienIndex + moving
+//   } else if (alienIndex === 29) {
+//     alienIndex += width
+//   } else if (alienIndex <= 39 && alienIndex > 30) {
+//     moving = -1
+//     alienIndex = alienIndex + moving
+//   } else if (alienIndex === 30) {
+//     alienIndex += width
+//   } else if (alienIndex < 49 && alienIndex >= 40) {
+//     moving = +1
+//     alienIndex = alienIndex + moving
+//   }
+//
+//   squares[alienIndex].classList.add('alien')
+// }
